65cdd44a38e2a32fee00a18e84bd0fd9dae90e9c
BAPPY AHMED <68845761+entbappy@users.noreply.github.com>
2023-08-05 20:12:19 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="helmet",
     version="0.0.1",
-    author="iNeuron",
-    author_email="cloud@ineuron.ai",
+    author="Bappy",
+    author_email="entbappy73@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
